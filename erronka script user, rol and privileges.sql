129fda6003c66a0558cef07f479fa1be8b64db1f
in1dm3-d <in1dm3-d@elorrieta.int>
2024-05-07 09:33:00 +0200

    user scripta eta rolak igota

--- erronka script user, rol and privileges.sql
+++ erronka script user, rol and privileges.sql
@@ -3,16 +3,16 @@
 -- Departamentu burua --> estadistikak, bezero eta premium taulan modifikazioak egin dezake
 
 	drop role if exists burua;
-    create role if not exists burua;
+    create role burua;
     grant select, insert, update, delete on db_spotify5.estatistikak to burua;
 	grant select, insert, update, delete on db_spotify5.bezero to burua;
 	grant select, insert, update, delete on db_spotify5.premium to burua;
-  
-    drop user if exists depburu;
-	CREATE USER if not exists 'depburu'@'%' identified by 'depPass';
-    GRANT burua TO 'depburu'@'%';
     
-    show grants for depburu;
+    drop user if exists departamentu_burua;
+	CREATE USER departamentu_burua identified by 'depPass';
+    
+	grant burua to departamentu_burua;
+    show grants for departamentu_burua;
 
 -- Analista --> Taula guztietako datuak ikusi dezake playlistabestiak eta hizkuntzak tauletan ez kontatuz
 
@@ -30,6 +30,9 @@
     grant select on db_spotify5.podcaster to analista;
 	grant select on db_spotify5.premium to analista;
     
+
+
+
 	drop user if exists analista1;
     create user analista1 identified by 'anaPass';
     
@@ -41,24 +44,13 @@
     drop user if exists analista2;
     Create user analista2 identified by 'anaPass';
 
-	grant select on db_spotify5.abestia to 'analista2';
-	grant select on db_spotify5.album to 'analista2';
-	grant select on db_spotify5.audioa to 'analista2';
-	grant select on db_spotify5.bezero to 'analista2';
-	grant select on db_spotify5.erreprodukzioak to 'analista2';
-	grant select on db_spotify5.estatistikak to 'analista2';
-	grant select on db_spotify5.gustokoak to 'analista2';
-	grant select on db_spotify5.musikaria to 'analista2';
-	grant select on db_spotify5.podcast to 'analista2';
-	grant select on db_spotify5.podcaster to 'analista2';
-	grant select on db_spotify5.premium to 'analista2';
-
+    grant analista to analista2;
 	show grants for analista2;
 
 -- Langilea --> Taula guztiak ikusi dezake gustokoak eta playlistabestiak izan ezik eta gainera hizkuntza taulako datuak aldatu ditzake
 	
     drop role if exists langileaR;
-    Create role langileaR ;
+    Create role langileaR;
 	
 	grant select on db_spotify5.abestia to langileaR;
 	grant select on db_spotify5.album to langileaR;
@@ -73,15 +65,14 @@
 	grant select on db_spotify5.premium to langileaR;
 	
     drop user if exists langilea;
-	create user 'langilea'@'%' identified by 'langPass';
+    Create user langilea identified by 'lanPass';
+    
+    show grants for langileaR;
 	grant langileaR to langilea;
+    show grants for langilea;
     
 -- Bezero --> Taula guztietan ,estatistikak izan ezin, ikusteko eta erreprodukzioak, gustokoak, premium, playlist eta playlistabestiak tauletan aldaketak egiteko ahalmena dauka
 
-
-	drop user if exists bezero;
-    create user 'bezero'@'%' identified by 'bezPass';
-    
 	drop role if exists bezeroR;
     create role bezeroR;
     
@@ -99,22 +90,16 @@
     grant select, insert, delete on db_spotify5.playlist to bezeroR;
     grant select, insert, delete on db_spotify5.playlistabestiak to bezeroR;
     
-    grant usage on *.* to bezeroR;
-    grant bezeroR to 'bezero'@'%';
-
-  
-	drop user if exists admin1;
-	drop user if exists b;
-     
-    create user 'admin1'@'%'identified by '1234';
-    grant all privileges on db_spotify5.* to admin1;
+    drop user if exists bezero;
+    create user bezero identified by 'bezPass';
     
-    
-    
-    create user'b'@'%' identified by'b';
-	grant all privileges on db_spotify5.* to b;
-    
+    grant bezeroR to bezero;
     show grants for bezeroR;
-    select * from mysql.user;
     
-    flush privileges;
+CREATE USER 'b'@'%' IDENTIFIED BY 'b';
+GRANT ALL PRIVILEGES ON db_spotify5.* TO 'b'@'%';
+FLUSH PRIVILEGES;
+
+show grants for b;
+DROP USER 'a'@'%';
+
